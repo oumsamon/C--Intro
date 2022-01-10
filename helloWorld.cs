143479cafd28398a6834d4eecc4dc55642b4f4f2
oumsamon <oumsamon@gmail.com>
2022-01-10 12:50:28 -0600

    basic declaring

---
+++ helloWorld.cs
@@ -0,0 +1,24 @@
+namespace IntroToCSharp
+{
+     //Commenting in C-Sharp
+        /* Multi Lines commenting*/
+    class Program
+    {
+        console.writeline("Hello World");
+
+/*Declaring Variables
+int
+string
+bool
+char
+
+*/
+        int myInt = 10;
+        int myInt2;
+
+        int myInt3, myInt4;
+
+        myInt2 = 6;
+
+    }
+}
